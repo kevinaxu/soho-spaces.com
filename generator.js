27ef71928e23eda3ef8b9d5121d7b92ae0e9bb4b
Kevin Xu <kevinxu16@gmail.com>
2024-02-28 09:07:02 -0500

    Update video tag

--- generator.js
+++ generator.js
@@ -22,7 +22,7 @@ function paragraph(heading, description) {
 function heroVideo(video_url) {
     return `
     <div class="hero-video-container h-96 m-4 rounded-lg">
-        <video id="hero-video" autoplay="" muted="" loop="" class="object-cover object-center h-full w-full lg:max-w-screen-lg rounded-lg">
+        <video id="hero-video" autoplay loop muted playsinline class="object-cover object-center h-full w-full lg:max-w-screen-lg rounded-lg">
             <source src="${video_url}" type="video/mp4">
         </video>
     </div>
