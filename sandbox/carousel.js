9222fa5d3ef43b637e6e09902940679ef1ca2733
Kevin Xu <kevinxu16@gmail.com>
2024-03-21 15:14:44 -0400

    Fix image carousel indicators

--- sandbox/carousel.js
+++ sandbox/carousel.js
@@ -13,9 +13,9 @@ function createThumbnailsComponent(photos) {
     
     // Step 1: Add thumbnails, carousel, and modal to the DOM
     const thumbnailsHTML = generateThumbnailsHTML(thumbnailsID, photos);
-    document.body.append(fromHTML(thumbnailsHTML));
     const carouselHTML   = generateCarouselHTML(carouselId, photos);
     const modalHTML      = generateModalHTML(modalId, carouselHTML);
+    document.body.append(fromHTML(thumbnailsHTML));
     document.body.append(fromHTML(modalHTML));
 
     // Step 2: Initialize Flowbite components
@@ -32,7 +32,7 @@ function createThumbnailsComponent(photos) {
 
 /**********************************************
  * 
- * Event Listeners 
+ * Step 3: Bind Event Listeners 
  * 
  *********************************************/
 
@@ -73,7 +73,7 @@ function bindThumbnailEventListeners(thumbnailsElement, flowbiteModal, flowbiteC
 
 /**********************************************
  * 
- * Flowbite Component Setup
+ * Step 2: Flowbite Component Setup
  * 
  *********************************************/
 
@@ -89,11 +89,12 @@ function initializeFlowbiteModal(modalId) {
         placement: 'bottom-right',
         backdrop: 'dynamic',
         backdropClasses:
-            'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+            // 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+            'fixed inset-0 z-40',
         closable: true,
-        // onHide:      () => { console.log('modal is hidden'); },
-        // onShow:      () => { console.log('modal is shown'); },
-        // onToggle:    () => { console.log('modal has been toggled'); },
+        onHide:      () => { console.log('modal is hidden'); },
+        onShow:      () => { console.log('modal is shown'); },
+        onToggle:    () => { console.log('modal has been toggled'); },
     };
 
     const instanceOptions = {
@@ -115,22 +116,33 @@ function initializeFlowbiteModal(modalId) {
 function initializeFlowbiteCarousel(id, photos) {
     const carouselElement = document.getElementById(id);
 
-    var items = [];
-    for (let i = 0; i < photos.length; i++) {
-        items.push({
-            position: i,
-            el: carouselElement.querySelectorAll(`[data-id=carousel-item-${i}]`)[0],
-        });
-    }
+    const items = photos.map((_, idx) => ({
+        position:   idx,
+        el:         carouselElement.querySelector(`[data-id=carousel-item-${idx}]`) 
+    }));
+    const indicators = photos.map((_, idx) => ({
+        position:   idx,
+        el:         carouselElement.querySelector(`[data-id=carousel-indicator-${idx}]`) 
+    }));
 
     const options = {
         defaultPosition: 1,
+        interval: 3000,
         indicators: {
-            activeClasses: 'bg-white dark:bg-gray-800',
+            activeClasses: 'bg-gray-950 dark:bg-gray-800',
             inactiveClasses:
                 'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
-            items: items,
-        }
+            items: indicators,
+        },
+        onNext: () => {
+            console.log('next slider item is shown');
+        },
+        onPrev: () => {
+            console.log('previous slider item is shown');
+        },
+        onChange: () => {
+            console.log('new slider item has been shown');
+        },    
     };
 
     const instanceOptions = {
@@ -145,7 +157,7 @@ function initializeFlowbiteCarousel(id, photos) {
 
 /**********************************************
  * 
- * HTML Generation Methods
+ * Step 1: HTML Generation Methods
  * 
  *********************************************/
 
@@ -204,18 +216,17 @@ function generateModalHTML(id, modalContent) {
  */
 function generateCarouselHTML(id, photos) {
     return `
-    <div id="${id}" class="relative w-full">
+    <div id="${id}" class="my-4 relative w-full">
+
         <!-- Carousel wrapper -->
-        <div
-            class="relative h-56 overflow-hidden rounded-lg sm:h-64 xl:h-80 2xl:h-96"
-        >
+        <div class="relative overflow-hidden" style="height: 32rem">
             ` + 
             photos.map((photo, idx) => {
                 return `
-                <div data-id="carousel-item-${idx}" class="hidden duration-700 ease-in-out">
+                <div data-id="carousel-item-${idx}" data-carousel-item class="hidden duration-700 ease-in-out">
                     <img
                         src="${photo}"
-                        class="absolute left-1/2 top-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
+                        class="absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                         alt="..."
                     />
                 </div>
@@ -224,15 +235,13 @@ function generateCarouselHTML(id, photos) {
             `
         </div>
         <!-- Slider indicators -->
-        <div
-            class="absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse"
-        >
+        <div class="absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse">
             ` + 
-            photos.map((photo, idx) => {
+            photos.map((_, idx)  => {
                 return `
                     <button
-                        data-id="carousel-indicator-${idx}"
                         type="button"
+                        data-id="carousel-indicator-${idx}"
                         class="h-3 w-3 rounded-full"
                         aria-current="true"
                         aria-label="Slide 1"
@@ -242,57 +251,17 @@ function generateCarouselHTML(id, photos) {
             `
         </div>
         <!-- Slider controls -->
-        <button
-            data-id="data-carousel-prev"
-            type="button"
-            class="group absolute left-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
-        >
-            <span
-                class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70"
-            >
-                <svg
-                    class="h-4 w-4 text-white dark:text-gray-800"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 6 10"
-                >
-                    <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M5 1 1 5l4 4"
-                    />
-                </svg>
-                <span class="hidden">Previous</span>
-            </span>
+        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-id="data-carousel-prev">
+            <svg class="w-4 text-gray-300 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M5 1 1 5l4 4"/>
+            </svg>
+            <span class="sr-only">Previous</span>
         </button>
-        <button
-            data-id="data-carousel-next"
-            type="button"
-            class="group absolute right-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
-        >
-            <span
-                class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70"
-            >
-                <svg
-                    class="h-4 w-4 text-white dark:text-gray-800"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 6 10"
-                >
-                    <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="m1 9 4-4-4-4"
-                    />
-                </svg>
-                <span class="hidden">Next</span>
-            </span>
+            <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-id="data-carousel-next">
+            <svg class="w-4 text-gray-300 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="m1 9 4-4-4-4"/>
+            </svg>
+            <span class="sr-only">Next</span>
         </button>
     </div>`;
 }
